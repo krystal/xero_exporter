6e4b103a2ade874910bb57eb6f5be92c94d7a7aa
Adam Cooke <adam@krystal.io>
2026-03-24 16:26:30 +0000

    chore: replace schmersion with release-please for release management
    Remove schmersion config and VERSION file logic. Hardcode version
    string in version.rb so release-please can manage it directly.
    Add release-please manifest config and update CI workflow.

--- lib/xero_exporter/version.rb
+++ lib/xero_exporter/version.rb
@@ -2,11 +2,6 @@
 
 module XeroExporter
 
-  VERSION_FILE_ROOT = File.expand_path('../../VERSION', __dir__)
-  if File.file?(VERSION_FILE_ROOT)
-    VERSION = File.read(VERSION_FILE_ROOT).strip.sub(/\Av/, '')
-  else
-    VERSION = '0.0.0.dev'
-  end
+  VERSION = '1.3.0'
 
 end
